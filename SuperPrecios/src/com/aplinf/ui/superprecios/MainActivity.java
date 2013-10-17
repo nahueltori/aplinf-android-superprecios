6c6a3221c98c054df3d98a0e39dcd5d77a6f756c
Nahuel Tori <nahueltori@gmail.com>
2013-10-16 21:17:56 -0300

    Primera versión de aplicación Android: escáner funcionando!!!

--- SuperPrecios/src/com/aplinf/ui/superprecios/MainActivity.java
+++ SuperPrecios/src/com/aplinf/ui/superprecios/MainActivity.java
@@ -2,14 +2,30 @@ package com.aplinf.ui.superprecios;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		scanBtn = (Button)findViewById(R.id.scan_button);
+		scanBtn.setOnClickListener(this);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
 	}
 
 	@Override
@@ -17,6 +33,33 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/**
+	 * Acceso a la interfaz de escaneo.
+	 */
+	public void onClick(View v){
+		if(v.getId()==R.id.scan_button){
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+		}
+	}
+	
+	/**
+	 * Resultado del escaneo. 
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanningResult != null) {
+			String scanContent = scanningResult.getContents();
+			String scanFormat = scanningResult.getFormatName();
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENT: " + scanContent);
+		}else{
+		    Toast toast = Toast.makeText(getApplicationContext(),
+		            "No se recibió información del escaneo", Toast.LENGTH_SHORT);
+		        toast.show();
+		}
 	}
 
 }
